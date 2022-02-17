9dded21a8cd7b60b3ee20831bebfc09c7371784a
ochyrkov <oleksandr.chyrkov@dxc.com>
2022-02-17 20:32:47 +0200

    JDSHIL-1500: playbook for mirroring stash-prod to stash-dev

--- vars/colored.groovy
+++ vars/colored.groovy
@@ -1,8 +1,8 @@
-def call(msg, color) {
 
-    if ( color == 'red' ) {
-        println("\033[0;31m${msg}\033[0m")
-    } else {
-        println("\033[0;32m${msg}\033[0m")
-    }
+def green(msg) {
+    println("\033[0;32m${msg}\033[0m")
+}
+
+def red(msg) {
+    println("\033[0;31m${msg}\033[0m")
 }
